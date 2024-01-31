4e1a86c19a7010777574d424635c2849dc28c35d
milayetria <158257084+milayetria@users.noreply.github.com>
2024-01-31 14:39:03 +0700

    Update cobaa.py

--- cobaa.py
+++ cobaa.py
@@ -8,7 +8,7 @@ st.subheader ('Plot')
 
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) 
 y = np.sin (x) # calculating sin (x) values
-z = np.scos (x) # calculating cos (x) values
+z = np.cos (x) # calculating cos (x) values
 
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b') #plotting sin(x) curve 
