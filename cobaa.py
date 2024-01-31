c7db60e6d45331879297c487c205d19332519001
milayetria <158257084+milayetria@users.noreply.github.com>
2024-01-31 15:02:20 +0700

    Update cobaa.py

--- cobaa.py
+++ cobaa.py
@@ -20,3 +20,40 @@ ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig)
+
+# subheader 
+st.subheader("Plot Sin & Cos ")
+
+coll. col2 = st.coloumn(2)
+
+with coll:
+st.caption('Plot Sin')
+x = np.linspace(-2 * np.pi,  1000)
+y = np.cos(x)
+
+fig, ax = plt.subplots(figsize=(16, 8)
+ax.plot(x, y, label='cos(x)', color='g')
+ax.set_ylabel("Cos x")
+ax.set_xlabel("x") 
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)                      
+
+st.pyplot(fig)
+
+with col2:
+st.caption('Plot Cos')
+x = np.linspace(-2 * np.pi,  1000)
+y = np.cos(x)
+
+fig, ax = plt.subplots(figsize=(16, 8)
+ax.plot(x, y, label='cos(x)', color='g')
+ax.set_ylabel("Cos x")
+ax.set_xlabel("x") 
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)                      
+
+st.pyplot(fig)
+
+st.caption('Copyright © Sarmila Yetria 2023')
