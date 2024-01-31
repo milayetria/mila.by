a47ac0bff689d34a377a24b8340bfcb41fb0070b
milayetria <158257084+milayetria@users.noreply.github.com>
2024-01-31 15:12:59 +0700

    Update cobaa.py

--- cobaa.py
+++ cobaa.py
@@ -31,7 +31,7 @@ with col1:
   x = np.linspace(-2 * np.pi,  1000)
   y = np.cos(x)
   
-  fig, ax = plt.subplots(figsize=(16, 8)
+  fig, ax = plt.subplots(figsize=(16, 8))
   ax.plot(x, y, label='sin(x)', color='b')
   ax.set_ylabel("Sin x")
   ax.set_xlabel("x") 
