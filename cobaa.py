40e666306178a83878846034efd5783ca574a756
milayetria <158257084+milayetria@users.noreply.github.com>
2024-01-31 14:11:27 +0700

    Create cobaa.py

---
+++ cobaa.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Header
+st.header('Sarmila Yetria :sparkles:')
+st.subheader ('Plot')
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) 
+y = np.sin (x) # calculating sin (x) values
+z = np.scos (x) # calculating cos (x) values
+
+fig, ax = plt.subplots(figsize=(16, 8)
+ax.plot(x, y , label='sin(x)', color='b') #plotting sin(x) curve 
+ax.plot(x, z , label='cos(x)', color='g') #plotting cods(x) curve 
+ax.set_ylabel("")
+ax.set_xlabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(), rotation =20, ha='right'
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
