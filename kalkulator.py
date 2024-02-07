ef039c550ff6102b6900540d20d921d44dc250a7
milayetria <158257084+milayetria@users.noreply.github.com>
2024-02-07 13:46:51 +0700

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Header 
+st.header('Sarmila :sparkles:')
+st.subheader('Plot')
+
+c1, c2 = st.columns(2)
+
+with c1:
+    x = st.number_input('Angka ',values=100)
+    st.write('=>: ')
+with c2:
+    operator = st.selectbox(
+      'operator',
+       ('+', '-', 'x', ':'), key='k1')
+    st.write('sparkles:')
+
+st.write(x, '',operator,' = ','')
+
+st.caption('Copyright © Sarmila Yetria 2023')
