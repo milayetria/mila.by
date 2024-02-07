a045a52ca0cd1761deb787ff0e384b9f986e2e1e
milayetria <158257084+milayetria@users.noreply.github.com>
2024-02-07 13:57:50 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -7,13 +7,13 @@ st.subheader('Plot')
 c1, c2 = st.columns(2)
 
 with c1:
-    x = st.number_input('Angka ',values=100)
+    x = st.number_input('Angka ',value=100)
     st.write('=>: ')
 with c2:
     operator = st.selectbox(
         'Operator',
         ('+', '-', 'x', ':'), key='k1')
-    st.write('sparkles:')
+    st.write(':sparkles: ')
 
 st.write(x, '',operator,' = ','')
 
