59e33e562626fac253568a19d3bdb5a76599ecf1
milayetria <158257084+milayetria@users.noreply.github.com>
2024-02-21 14:40:46 +0700

    Update slider.py

--- slider.py
+++ slider.py
@@ -35,16 +35,23 @@ ax.tick_params(axis='x', labelsize=15)
 plt.grid(color='green', linestyle='-.', linewidth=.5)
 st.pyplot(fig)
 
-def f(x):
+def fungsi(x):
     return 13 * x**2 + 9 * x - 12
 
-u = f(t)dx
-fig, ax = plt.subplots(figsize=(16,8))
-ax.plot(t, u, label='sin(t)', color='b') 
-ax.set_ylabel("")
-ax.set_xlabel("t")
-ax.tick_params(axis='y', labelsize=20)
-ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
-ax.tick_params(axis='x', labelsize=15)
-plt.grid(color='green', linestyle='-.', linewidth=.5)
-st.pyplot(fig)
+def integral_trapesium(x1, x2, n):
+    h = (x2 - x1) / n
+    integral = 0.5 * (fungsi(x1) + fungsi(x2))
+
+    for i in range(1, n):
+        x = x1 + i * h
+        integral += fungsi(x)
+
+    return h * integral
+
+x1 = 0
+x2 = 2
+n = 1000  # Jumlah trapesium, semakin besar semakin akurat
+
+hasil_integral = integral_trapesium(x1, x2, n)
+
+print(f"Integral dari fungsi 13x^2 + 9x - 12 dari {x1} hingga {x2} adalah: {hasil_integral}")
